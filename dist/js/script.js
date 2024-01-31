f660cacd41d97b58d845ec700f621a2ccd12e43e
Anas ER-RAKIBI <115924206+anas-er25@users.noreply.github.com>
2024-01-31 19:56:53 +0100

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,3 +3,12 @@ const mobileMenu = document.querySelector("#mobile-menu");
 mobileBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+var dateElements = document.getElementsByClassName("date");
+
+var currentYear = new Date().getFullYear();
+
+for (var i = 0; i < dateElements.length; i++) {
+  var dateString = "Copyright © " + currentYear + ", All Rights Reserved";
+  dateElements[i].innerHTML = dateString;
+}
